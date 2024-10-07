37f1edf9fcbb64dd437e3596dfac898b85f3d1e0
lbin719 <576838217@qq.com>
2024-10-07 16:45:26 +0800

    ref |> ulog增加互斥锁

--- Drivers/BSP/Components/ulog/ulog.c
+++ Drivers/BSP/Components/ulog/ulog.c
@@ -4,6 +4,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "cmsis_os.h"
 
 #ifdef  LOG_DEBUG_ENABLE
 
@@ -11,10 +12,15 @@
 
 static char ulog_buf[ULOGGER_BUFFER_SIZE];
 static bool ulog_is_init = false;
+static osMutexId ulog_osMutexHandle = NULL;
 
 void ulog_init(void)
 {
     uart1_init();
+
+    osMutexDef(ulog_osMutex);
+    ulog_osMutexHandle = osMutexCreate(osMutex(ulog_osMutex));
+    assert_param(ulog_osMutexHandle);
 
     ulog_is_init = true;
 }
@@ -28,6 +34,11 @@ int ulog_sync_output(bool output_tick, const char* format, ...)
         return 0;
     }
 
+    if (__get_IPSR() == 0)
+    {
+        osMutexWait(ulog_osMutexHandle, osWaitForever);
+    }
+
     if(output_tick)
     {
         cnt = snprintf(ulog_buf, sizeof(ulog_buf), "[%ld]", HAL_GetTick());
@@ -39,6 +50,12 @@ int ulog_sync_output(bool output_tick, const char* format, ...)
     va_end(argptr);
 
     uart1_sync_output((uint8_t *)ulog_buf, cnt);
+
+    if (__get_IPSR() == 0)
+    {
+        osMutexRelease(ulog_osMutexHandle);
+    }
+
     return cnt;
 }
 
