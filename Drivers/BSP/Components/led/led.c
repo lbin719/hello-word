f49565b93c41e198dc1e341cf8d6560b6e7d7c48
lbin719 <576838217@qq.com>
2024-10-07 17:30:11 +0800

    ref |> led 接口完善

--- Drivers/BSP/Components/led/led.c
+++ Drivers/BSP/Components/led/led.c
@@ -2,12 +2,44 @@
 #include "ulog.h"
 #include "stm32f1xx_hal.h"
 #include "board.h"
+#include "cmsis_os.h"
 
 #define LED_RED_ON()        	    (LED_RED_GPIO_PORT->BSRR = LED_RED_GPIO_PIN)
 #define LED_RED_OFF()       		(LED_RED_GPIO_PORT->BSRR = (uint32_t)(LED_RED_GPIO_PIN  << 16u))
 
 #define LED_GREEN_ON()        	    (LED_GREEN_GPIO_PORT->BSRR = (uint32_t)(LED_GREEN_GPIO_PIN  << 16u))
 #define LED_GREEN_OFF()       		(LED_GREEN_GPIO_PORT->BSRR = LED_GREEN_GPIO_PIN)
+
+static osTimerId led_timehandle = NULL;
+static bool led_status = false;
+
+static void led_ostimercallback(void const * argument)
+{
+    (void) argument;
+
+    if(!led_status)
+        return ;
+
+    HAL_GPIO_TogglePin(LED_RED_GPIO_PORT, LED_RED_GPIO_PIN);
+}
+
+static void led_control(bool on, uint32_t timer)
+{
+    led_status = on;
+    if(led_status)
+    {
+        if(timer > 0)
+        	osTimerStart(led_timehandle, timer);
+        else
+        	osTimerStop(led_timehandle);
+        LED_RED_ON();
+    }
+    else
+    {
+        osTimerStop(led_timehandle);
+        LED_RED_OFF();
+    }
+}
 
 void led_init(void)
 {
@@ -28,23 +60,10 @@ void led_init(void)
     gpio_init_struct.Pin = LED_GREEN_GPIO_PIN;
     HAL_GPIO_Init(LED_GREEN_GPIO_PORT, &gpio_init_struct);
 
+    osTimerDef(led_timer, led_ostimercallback);
+    led_timehandle = osTimerCreate(osTimer(led_timer), osTimerPeriodic, NULL);
+    assert_param(led_timehandle);
+
+    led_control(true, 50);
     LOG_I("%s\r\n", __FUNCTION__);
-}
-
-void led_task_handle(void)
-{
-    static bool led_test;
-
-    if(led_test)
-    {
-        LED_RED_OFF();
-        LED_GREEN_OFF();
-    }
-    else
-    {
-        LED_RED_ON();
-        LED_GREEN_ON();
-    }
-
-    led_test = !led_test;
 }
--- Drivers/BSP/Components/led/led.h
+++ Drivers/BSP/Components/led/led.h
@@ -2,7 +2,7 @@
 #define _LED_H
 
 void led_init(void);
-void led_task_handle(void);
+
 
 #endif /* _LED_H */
 
